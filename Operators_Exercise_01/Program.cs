52b3bd217c7b24aa826e0bbd0edefcb7618e5910
johndward01 <johndward01@gmail.com>
2023-03-06 11:41:38 -0600

    done

--- Operators_Exercise_01/Program.cs
+++ Operators_Exercise_01/Program.cs
@@ -5,31 +5,31 @@ var sum = 0;
 
 
 // TODO: Increment the variable by 1 using the pre-increment operator
-
+Console.WriteLine(++sum); 
 
 // TODO: Increment the variable by 1 using the post-increment operator
-
+Console.WriteLine(sum++); 
 
 // TODO: Decrement the variable by 1 using the pre-decrement operator
-
+Console.WriteLine(--sum); 
 
 // TODO: Decrement the variable by 1 using the post-decrement operator
-
+Console.WriteLine(sum--); 
 
 // TODO: Add 1000 to the variable
-
+Console.WriteLine(sum+=1000); 
 
 // TODO: Subtract 500 from the variable
-
+Console.WriteLine(sum-=500); 
 
 // TODO: Multiply the variable by 4
-
+Console.WriteLine(sum*=4); 
 
 // TODO: Divide the variable by 2
-
+Console.WriteLine(sum/=2); 
 
 // TODO: Modulo the variable by 2
-
+Console.WriteLine(sum%=2); 
 
 // BONUS
 int a = 4;
@@ -39,3 +39,4 @@ int d = 1;
 
 var answer = ++a + b++ - --c * d--;
 // TODO: Try to figure out what answer will be, before you print it out to the console
+Console.WriteLine(answer); 
