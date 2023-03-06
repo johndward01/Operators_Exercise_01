7bdf791d03fb72535e4272b5576a71175909e08b
johndward01 <johndward01@gmail.com>
2023-03-06 11:35:30 -0600

    Add project files.

---
+++ Operators_Exercise_01/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// For all TODO's use the sum variable declared below and also print it to the console
+// Example:    Console.WriteLine(sum += 3000);
+var sum = 0;
+
+
+// TODO: Increment the variable by 1 using the pre-increment operator
+
+
+// TODO: Increment the variable by 1 using the post-increment operator
+
+
+// TODO: Decrement the variable by 1 using the pre-decrement operator
+
+
+// TODO: Decrement the variable by 1 using the post-decrement operator
+
+
+// TODO: Add 1000 to the variable
+
+
+// TODO: Subtract 500 from the variable
+
+
+// TODO: Multiply the variable by 4
+
+
+// TODO: Divide the variable by 2
+
+
+// TODO: Modulo the variable by 2
+
+
+// BONUS
+int a = 4;
+int b = 3;
+int c = 2;
+int d = 1;
+
+var answer = ++a + b++ - --c * d--;
+// TODO: Try to figure out what answer will be, before you print it out to the console
